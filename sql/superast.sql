c426d38db1b15390fc520fb1ee07da2982a3e9a7
William Urbano <contato@williamurbano.com.br>
2013-11-27 13:46:33 -0200

    Estrutura de banco de dados
    Adicionada coluna menu_route à tabela menu para armazenar o nome da rota a qual o menu pertence.
    Tabela firm renomeada para company.
    Adicionada coluna user_company à tabela user para indicar a empresa a qual o usuário faz parte.
    
    Signed-off-by: William Urbano <contato@williamurbano.com.br>

--- sql/superast.sql
+++ sql/superast.sql
@@ -10,18 +10,43 @@ CREATE TABLE `menu` (
     `menu_module` VARCHAR(255),
     `menu_controller` VARCHAR(255),
     `menu_action` VARCHAR(255),
+    `menu_route` VARCHAR(255),
     `menu_data_target` VARCHAR(255),
     `menu_icon_class` VARCHAR(255),
-    `menu_parent` INT(10) UNSIGNED
-)  ENGINE=InnoDB;
+    `menu_parent` INT(10) UNSIGNED,
+    `menu_order` INT
+)  ENGINE=InnoDB AUTO_INCREMENT=22;
 
-CREATE TABLE `firm` (
-    `firm_id` INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `firm_name` VARCHAR(255),
-    `firm_active` TINYINT(1)
+INSERT INTO `menu` (`menu_id`, `menu_name`, `menu_description`, `menu_page_title`, `menu_module`, `menu_controller`, `menu_action`, `menu_route`, `menu_data_target`, `menu_icon_class`, `menu_parent`, `menu_order`) VALUES
+(1, 'Dashboard', 'Resumo de informaÃ§Ãµes da aplicaÃ§Ã£o', 'Dashboard', 'default', 'index', 'index', 'dashboard_action', '', 'dashboard', NULL, 1),
+(2, 'AplicaÃ§Ãµes', '', '', '', '', '', 'index_action', '#sidebar_aplicacoes', 'grid layout', NULL, 2),
+(3, 'ConfiguraÃ§Ãµes', 'ConfiguraÃ§Ãµes do sistema', '', '', '', '', 'index_action', '#sidebar_configuracoes', 'settings', NULL, 3),
+(4, 'CRM', 'Relacionamento com o cliente', 'CRM', '', '', '', 'index_action', '', 'suitcase', 2, 1),
+(5, 'Clientes', 'Gerenciamento de clientes', 'Clientes', 'crm', 'customers', 'index', 'index_action', '', 'briefcase', 4, 1),
+(6, 'Atendimentos', 'Contatos realizados com os clientes', 'Atendimentos', 'crm', 'attendances', 'index', 'index_action', '', 'headphones', 4, 2),
+(7, 'Meus agendamentos', 'Agendamento de contatos a serem realizados com os clientes', 'Meus agendamentos', 'crm', 'schedules', 'index', 'index_action', '', 'calendar', 4, 3),
+(8, 'RelatÃ³rios', 'RelatÃ³rios de informaÃ§Ãµes de clientes', 'RelatÃ³rios', 'crm', 'reports', 'index', 'index_action', '', 'chart basic', 4, 4),
+(9, 'Telefonia', 'Gerenciamento do Asterisk', 'Telefonia', '', '', '', 'index_action', '', 'phone', 2, 2),
+(10, 'Servidores', 'Gerenciamento dos servidores Asterisk cadastrados.', 'Servidores', 'telephony', 'servers', 'index', 'index_action', '', 'hdd', 9, 1),
+(11, 'Filas', 'Gerenciamento das filas de atendimento do Asterisk', 'Filas', 'telephony', 'queues', 'index', 'index_action', '', 'ellipsis vertical', 9, 2),
+(12, 'Ramais', 'Gerenciamento dos ramais do Asterisk', 'Ramais', 'telephony', 'extensions', 'index', 'index_action', '', 'phone', 9, 3),
+(13, 'Campanhas', 'Gerenciamento de campanhas ativas e receptivas das filas de atendimento', 'Campanhas', 'telephony', 'campaigns', 'index', 'index_action', '', 'exchange', 9, 4),
+(14, 'Monitoramento', 'Monitoramento dos serviÃ§os de telefonia', 'Monitoramento', 'telephony', 'monitoring', 'index', 'index_action', '', 'tasks', 9, 5),
+(15, 'RelatÃ³rios', 'RelatÃ³rios de informaÃ§Ãµes de telefonia', 'RelatÃ³rios', 'telephony', 'reports', 'index', 'index_action', '', 'chart basic', 9, 7),
+(16, 'TarifaÃ§Ã£o', 'ConferÃªncia de custos de telefonia', 'TarifaÃ§Ã£o', 'telephony', 'billing', 'index', 'index_action', '', 'money', 9, 6),
+(17, 'Acessos', 'Gerenciamento do controle de acesso de usuÃ¡rios', 'Acessos', '', '', '', 'index_action', '', 'community basic', 3, 1),
+(18, 'Empresas', 'Gerenciamento de empresas de usuÃ¡rios cadastrados', 'Empresas', 'default', 'companies', 'index', 'settings_index_action', '', 'building', 17, 1),
+(19, 'Grupos', 'Gerenciamento de grupos de usuÃ¡rios', 'Grupos', 'default', 'groups', 'index', 'settings_index_action', '', 'users', 17, 2),
+(20, 'UsuÃ¡rios', 'Gerenciamento de usuÃ¡rios do sistema', 'UsuÃ¡rios', 'default', 'users', 'index', 'settings_index_action', '', 'user', 17, 3),
+(21, 'Menus', 'Gerenciamento de Ã¡reas desenvolvidas disponÃ­veis do sistema', 'Menus', 'default', 'menus', 'index', 'settings_index_action', '', 'reorder', 17, 4);
+
+CREATE TABLE `company` (
+    `company_id` INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `company_name` VARCHAR(255),
+    `company_active` TINYINT(1)
 )  ENGINE=InnoDB AUTO_INCREMENT=2;
 
-INSERT INTO `firm` (`firm_id`, `firm_name`, `firm_active`) VALUES
+INSERT INTO `company` (`company_id`, `company_name`, `company_active`) VALUES
 (1, 'Scitech', 1);
 
 CREATE TABLE `role` (
@@ -39,11 +64,12 @@ CREATE TABLE `user` (
     `user_login` VARCHAR(255),
     `user_password` VARCHAR(255),
     `user_role` INT(10) UNSIGNED,
+	`user_company` INT(10) UNSIGNED,
     `user_active` TINYINT(1) DEFAULT 1
 )  ENGINE=InnoDB AUTO_INCREMENT=2;
 
-INSERT INTO `user` (`user_id`, `user_name`, `user_login`, `user_password`, `user_role`, `user_active`) VALUES
-(1, 'William Urbano', 'william', MD5(123), 1, 1);
+INSERT INTO `user` (`user_id`, `user_name`, `user_login`, `user_password`, `user_role`, `user_company`, `user_active`) VALUES
+(1, 'William Urbano', 'william', MD5(123), 1, 1, 1);
 
 CREATE TABLE `group` (
     `group_id` INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -63,12 +89,6 @@ CREATE TABLE `user_group` (
 INSERT INTO `user_group` (`user_group_id`, `user_group_user`, `user_group_group`) VALUES 
 (1, 1, 1);
 
-CREATE TABLE `user_firm` (
-    `user_firm_id` INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `user_firm_user` INT(10) UNSIGNED NOT NULL,
-    `user_firm_firm` INT(10) UNSIGNED NOT NULL
-)  ENGINE=InnoDB;
-
 CREATE TABLE `group_menu` (
     `group_menu_id` INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `group_menu_group` INT(10) UNSIGNED NOT NULL,
@@ -85,18 +105,20 @@ CREATE TABLE `server` (
     `server_ami_secret` VARCHAR(255),
     `server_is_elastix` TINYINT(1) DEFAULT 0,
     `server_active` TINYINT(1) DEFAULT 1
-)  ENGINE=InnoDB;
+)  ENGINE=InnoDB AUTO_INCREMENT=4;
+
+INSERT INTO `server` (`server_id`, `server_hostname`, `server_ip_address`, `server_database_user`, `server_database_password`, `server_ami_user`, `server_ami_secret`, `server_is_elastix`, `server_active`) VALUES
+(1, 'callcenter', '10.0.0.18', 'root', 'sci134!', 'admin', 'sci134', 1, 1),
+(2, 'zincornio', '10.0.0.132', 'root', 'ultima91', 'admin', '123123', 1, 1),
+(3, 'voip', '10.0.0.1', 'root', 'MySQL987', 'admin', 'Pbx987Linux', 1, 1);
 
 ALTER TABLE `user`
-ADD CONSTRAINT `fk_user_role` FOREIGN KEY (`user_role`) REFERENCES `role`(`role_id`) ON UPDATE CASCADE ON DELETE NO ACTION;
+ADD CONSTRAINT `fk_user_role` FOREIGN KEY (`user_role`) REFERENCES `role`(`role_id`) ON UPDATE CASCADE ON DELETE NO ACTION,
+ADD CONSTRAINT `fk_user_company` FOREIGN KEY (`user_company`) REFERENCES `company`(`company_id`) ON UPDATE CASCADE ON DELETE NO ACTION;
 
 ALTER TABLE `user_group`
 ADD CONSTRAINT `fk_user_group_user` FOREIGN KEY (`user_group_user`) REFERENCES `user`(`user_id`) ON UPDATE CASCADE ON DELETE CASCADE,
 ADD CONSTRAINT `fk_user_group_group` FOREIGN KEY (`user_group_group`) REFERENCES `group`(`group_id`) ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE `user_firm`
-ADD CONSTRAINT `fk_user_firm_user` FOREIGN KEY (`user_firm_user`) REFERENCES `user`(`user_id`) ON UPDATE CASCADE ON DELETE CASCADE,
-ADD CONSTRAINT `fk_user_firm_firm` FOREIGN KEY (`user_firm_firm`) REFERENCES `firm`(`firm_id`) ON UPDATE CASCADE ON DELETE CASCADE;
 
 ALTER TABLE `group_menu`
 ADD CONSTRAINT `fk_group_menu_group` FOREIGN KEY (`group_menu_group`) REFERENCES `group`(`group_id`) ON UPDATE CASCADE ON DELETE CASCADE,
