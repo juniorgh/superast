3aafe768176ba29a67dff86a96a1ce7eaaea1bdb
William Urbano <contato@williamurbano.com.br>
2013-11-27 16:17:35 -0200

    Gerenciamento de cargos
    Criado gerenciamento de cargos exercidos pelos usuários.
    
    Signed-off-by: William Urbano <contato@williamurbano.com.br>

---
+++ var/www/application/modules/default/controllers/RolesController.php
@@ -0,0 +1,75 @@
+<?php
+
+class Default_RolesController extends Zend_Controller_Action {
+
+    public function indexAction() {
+        $where = null;
+        $order = null;
+        $hasFilter = false;
+        $params = $this->getRequest()->getParams();
+
+        $where = array('role_active = 1');
+
+        if(!empty($params['role_name'])) {
+            $where[] = "role_name LIKE '%{$params['role_name']}%'";
+            $hasFilter = true;
+        }
+
+        $query = Default_Model_Role::read(null, true, $where, $order);
+        $paginator = Zend_Paginator::factory($query);
+        $paginator->setCurrentPageNumber(parent::_getParam('page', 1));
+        $paginator->setItemCountPerPage(10);
+        $paginator->setPageRange(5);
+
+        $this->view->assign('roles', $paginator);
+        $this->view->assign('hasFilter', $hasFilter);
+    }
+
+    public function viewAction() {
+        $id = $this->getRequest()->getParam('id', null);
+        if(!is_null($id)) {
+            $role = Default_Model_Role::read($id);
+            $this->view->assign('role', $role);
+        }
+    }
+
+    public function saveAction() {
+        $this->_helper->viewRenderer->setNoRender();
+        $this->_helper->layout->disableLayout();
+
+        $request = $this->getRequest();
+        if($request->isPost()) {
+            $params = $request->getPost();
+            if(!array_key_exists('role_id', $params)) {
+                Default_Model_Role::create($params);
+            } else {
+                Default_Model_Role::update($params);
+            }
+        }
+        
+        $this->_redirect($this->view->actions['index']);
+    }
+
+    public function dropAction() {
+        $id = $this->getRequest()->getParam('id', null);
+        if(!is_null($id)) {
+            $data = array(
+                'role_id' => $id,
+                'role_active' => 0
+            );
+
+            $role = Default_Model_Role::update($data);
+
+            $this->_redirect($this->view->actions['index']);
+        }
+    }
+
+    public function formAction() {
+        $id = $this->getRequest()->getParam('id', null);
+        if(!is_null($id)) {
+            $role = Default_Model_Role::read($id);
+            $this->view->assign('role', $role);
+        }
+    }
+
+}
---
+++ var/www/application/modules/default/models/Role.php
@@ -0,0 +1,50 @@
+<?php
+
+class Default_Model_Role {
+
+    public static function create($data) {
+        $role = new Default_Model_DbTable_Role();
+        return $role->insert($data);
+    }
+
+    public static function read($id = null, $pager = false, $where = null, $order = null) {
+        $role = new Default_Model_DbTable_Role();
+        if(!is_null($id)) {
+            return $role->find($id)->current()->toArray();
+        } else {
+            $query = $role->select();
+            if(!is_null($where)) {
+                foreach($where as $cond) {
+                    $query->where($cond);
+                }
+            }
+
+            if(!is_null($order)) {
+                $query->order($order);
+            } else {
+                $query->order('role_name');
+            }
+
+            if($pager == false) {
+                return $role->fetchAll($query);
+            } else {
+                return $query;
+            }
+        }
+    }
+
+    public static function update($data) {
+        $role = new Default_Model_DbTable_Role();
+        $where = $role->getAdapter()->quoteInto('role_id = ?', $data['role_id']);
+        unset($data['role_id']);
+        return $role->update($data, $where);
+    }
+
+    public static function delete($id) {
+        $role = new Default_Model_DbTable_Role();
+        $where = $role->getAdapter()->quoteInto('role_id = ?', $id);
+        return $role->delete($where);
+    }
+
+}
+
